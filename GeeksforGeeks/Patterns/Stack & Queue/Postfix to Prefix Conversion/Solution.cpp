978d709541e3657b8f5fa5f7308b1cd8f5800281
vedantmagarde <vedantmagarde@gmail.com>
2026-08-06 01:00:49 +0530

    Add C++ Solution to Patterns/Stack & Queue

---
+++ GeeksforGeeks/Patterns/Stack & Queue/Postfix to Prefix Conversion/Solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+	public:
+	
+	string postToPre(string s) {
+		string ans = "";
+		stack<string>st;
+		int i = 0;
+		while (i <s.length()) {
+			
+			if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '^') {
+				string s1 = st.top();
+				st.pop();
+				string s2 = st.top();
+				st.pop();
+				string final = s[i] + s2 + s1;
+				st.push(final);
+			}
+			
+			
+			else {
+				string temp = "";
+				temp += s[i];
+				st.push(temp);
+			}
+			
+			i++;
+		}
+		
+		while (!st.empty()) {
+			ans += st.top();
+			st.pop();
+		}
+		
+		return ans;
+		
+	}
+};
